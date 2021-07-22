913af8237882283b5a3b155fbed02f9deac579e1
Hrishikesh Shinde <hrishi.shinde.20@gmail.com>
2021-07-22 18:12:39 +0530

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
@@ -21,15 +22,25 @@ if(process.env.NODE_ENV === 'development') {
 
 app.use(express.json())
 
-app.get('/', (req,res) => {
-    res.send('API is Running..')
-})
+
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 
 app.get('/api/config/paypal', (req,res) => res.send(process.env.PAYPAL_CLIENT_ID))
+
+
+const __dirname = path.resolve()
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend','build','index.html')))
+}
+else {
+    app.get('/', (req,res) => {
+        res.send('API is Running..')
+    })
+}
 
 app.use(notFound)
 app.use(errorHandler)
